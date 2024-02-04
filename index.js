ba9e0cbe969733c9aab7f426994ef247932f50dd
amna <amnamh2000@gmail.com>
2024-02-04 03:18:34 +0300

    task2

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//hw 1
+//task1
+
 var myName = 'Amna';
 var myAge = 23;
 var isStudent = true;
@@ -5,3 +8,13 @@ var isStudent = true;
 console.log('Name:',myName);
 console.log('Age:',myAge);
 console.log( 'Am I student in sct?',isStudent);
+
+//task2
+
+var ToCheck1= 'mnmn';
+var ToCheck2 = 11;
+var ToCheck3 = true;
+console.log( 'type of Tocheck1:',typeof ToCheck1);
+console.log( 'type of Tocheck2:',typeof ToCheck2);
+console.log( 'type of Tocheck3:',typeof ToCheck3);
+
